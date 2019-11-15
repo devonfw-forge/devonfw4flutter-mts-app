ff2b477ba96fb56091b54fc3d058d953788d4ccf
Fasust <sebastianf333@googlemail.com>
2019-11-15 17:04:18 +0100

    SliverSearchHeader

--- lib/ui/pages/menu/menu.dart
+++ lib/ui/pages/menu/menu.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_thai_star_flutter/models/dish.dart';
 import 'package:my_thai_star_flutter/ui/pages/menu/dish_card.dart';
+import 'package:my_thai_star_flutter/ui/pages/menu/sliver_search_header.dart';
 import 'package:my_thai_star_flutter/ui/shared_widgets/app_drawer.dart';
 import 'package:my_thai_star_flutter/ui/shared_widgets/custom_app_bar.dart';
-import 'package:my_thai_star_flutter/ui/ui_helper.dart';
 
 class Menu extends StatelessWidget {
   final List<Dish> dishes = [
@@ -42,70 +42,7 @@ class Menu extends StatelessWidget {
       drawer: AppDrawer(),
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverAppBar(
-            elevation: UiHelper.elevation,
-            floating: true,
-            backgroundColor: Colors.white,
-            leading: new Container(),
-            expandedHeight: 120,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                color: Colors.white,
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        prefixIcon: Icon(Icons.search),
-                        labelText: "Search our Dishes",
-                      ),
-                    ),
-                    
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Icon(
-                            Icons.sort,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Text(
-                          "Sort by",
-                          style: Theme.of(context).textTheme.subhead,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Expanded(
-                          child: DropdownButton<String>(
-                            isExpanded: true,
-                            value: "Name",
-                            items: <String>['Price', 'Likes', 'Name']
-                                .map((String value) {
-                              return new DropdownMenuItem<String>(
-                                value: value,
-                                child: new Text(value),
-                              );
-                            }).toList(),
-                            onChanged: (_) {},
-                          ),
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.vertical_align_top,
-                            color: Theme.of(context).accentColor,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          SliverSearchHeader(),
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
@@ -121,3 +58,4 @@ class Menu extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/ui/pages/menu/sliver_search_header.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import '../../ui_helper.dart';
+
+class SliverSearchHeader extends StatelessWidget {
+  static const double height = 120;
+
+  const SliverSearchHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      elevation: UiHelper.elevation,
+      floating: true,
+      backgroundColor: Colors.white,
+      leading: new Container(),
+      expandedHeight: height,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              _SearchBar(),
+              _Sort(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatelessWidget {
+  const _SearchBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        prefixIcon: Icon(Icons.search),
+        labelText: "Search our Dishes",
+      ),
+    );
+  }
+}
+
+class _Sort extends StatelessWidget {
+  const _Sort({
+    Key key,
+  }) : super(key: key);
+
+  static const double iconPadding = 12.0;
+  static const double dropDownPadding = 5;
+  static const List<String> sortCriteria = ['Name', 'Price', 'Likes'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(iconPadding),
+          child: Icon(
+            Icons.sort,
+            color: Colors.grey,
+          ),
+        ),
+        Text(
+          "Sort by",
+          style: Theme.of(context).textTheme.subhead,
+        ),
+        SizedBox(
+          width: dropDownPadding,
+        ),
+        Expanded(
+          child: DropdownButton<String>(
+            isExpanded: true,
+            value: sortCriteria[0],
+            items: sortCriteria.map((String value) {
+              return new DropdownMenuItem<String>(
+                value: value,
+                child: new Text(value),
+              );
+            }).toList(),
+            onChanged: (_) {},
+          ),
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.vertical_align_top,
+            color: Theme.of(context).accentColor,
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}

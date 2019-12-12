c87c2ed35db1102d512d0ebd20e92253eed4acc8
Fasust <sebastianf333@googlemail.com>
2019-12-12 15:03:00 +0100

    SimpleBlocDelegate

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_thai_star_flutter/features/current_order/blocs/current_order_bloc.dart';
 import 'package:my_thai_star_flutter/router.dart';
+import 'package:my_thai_star_flutter/simple_bloc_delegate.dart';
+import 'package:bloc/bloc.dart';
 import 'theme.dart';
 
-void main() => runApp(MyThaiStar());
+void main() {
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(MyThaiStar());
+}
 
 class MyThaiStar extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
--- lib/simple_bloc_delegate.dart
+++ lib/simple_bloc_delegate.dart
@@ -3,13 +3,19 @@ import 'package:bloc/bloc.dart';
 class SimpleBlocDelegate extends BlocDelegate {
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
+    print(bloc.runtimeType.toString() +
+        ":\t" +
+        transition.currentState.runtimeType.toString() +
+        " + " +
+        transition.event.runtimeType.toString() +
+        " -> " +
+        transition.nextState.runtimeType.toString());
     super.onTransition(bloc, transition);
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stacktrace) {
-    print(error);
+    print(bloc.runtimeType.toString() + ":\t" + error);
     super.onError(bloc, error, stacktrace);
   }
 }

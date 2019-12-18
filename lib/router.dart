6821d06de6dbea2dc3e18125d5683b917daf750b
Fasust <sebastianf333@googlemail.com>
2019-12-18 15:07:40 +0100

    Set page transitions using package

--- lib/router.dart
+++ lib/router.dart
@@ -4,6 +4,7 @@ import 'package:my_thai_star_flutter/ui/booking/booking_page.dart';
 import 'package:my_thai_star_flutter/ui/current_order/current_order_page.dart';
 import 'package:my_thai_star_flutter/ui/home/home_page.dart';
 import 'package:my_thai_star_flutter/ui/menu/menu_page.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Router {
   static const String home = '/';
@@ -14,15 +15,31 @@ class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case home:
-        return MaterialPageRoute(builder: (_) => HomePage());
+        return PageTransition(
+          child: HomePage(),
+          type: PageTransitionType.fade,
+          settings: settings,
+        );
       case booking:
-        return MaterialPageRoute(builder: (_) => BookingPage());
+        return PageTransition(
+          child: BookingPage(),
+          type: PageTransitionType.fade,
+          settings: settings,
+        );
       case menu:
-        return MaterialPageRoute(builder: (_) => MenuPage());
+        return PageTransition(
+          child: MenuPage(),
+          type: PageTransitionType.fade,
+          settings: settings,
+        );
       case currentOrder:
-        return CupertinoPageRoute(builder: (_) => CurrentOrderPage());
+        return PageTransition(
+          child: CurrentOrderPage(),
+          type: PageTransitionType.rightToLeft,
+          settings: settings,
+        );
       default:
-        return MaterialPageRoute(builder: (_) => HomePage());
+        return null;
     }
   }
 }

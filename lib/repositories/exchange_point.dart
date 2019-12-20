c735723953bef421037093dc683a2102436162a8
Fasust <sebastianf333@googlemail.com>
2019-12-20 17:00:09 +0100

    Document repositories

--- lib/repositories/exchange_point.dart
+++ lib/repositories/exchange_point.dart
@@ -1,3 +1,7 @@
+///Use-case independant interface that exchanges 
+///[Input] for [Output] asynchronously
 abstract class ExchangePoint<Input, Output> {
+
+  ///Exchange one [Input] for a [Future<Output>]
   Future<Output> post(Input input);
 }

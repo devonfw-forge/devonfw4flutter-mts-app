2e12e255abebe46b4ab279d7efa098fd0705e06f
Fasust <sebastianf333@googlemail.com>
2019-12-20 19:33:59 +0100

    Document Localization

--- lib/localization.dart
+++ lib/localization.dart
@@ -4,6 +4,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
+///Generates and then provides a set [Translation] objects to the WidgetTree
+///
+///Flutters in-build way of handling Localization is through [LocalizationsDelegate]s.
+///We can create a custom one for out localized texts.
 class MtsLocalizationDelegate extends LocalizationsDelegate<Translation> {
   static const List<String> supportedLanguages = [
     'en',
@@ -20,6 +24,7 @@ class MtsLocalizationDelegate extends LocalizationsDelegate<Translation> {
   bool isSupported(Locale locale) =>
       supportedLanguages.contains(locale.languageCode.toLowerCase());
 
+  ///Generates on [Translation] for a given [Locale]
   @override
   Future<Translation> load(Locale locale) async {
     Map<dynamic, dynamic> translationMap = await _loadFromAssets(locale);
@@ -29,6 +34,8 @@ class MtsLocalizationDelegate extends LocalizationsDelegate<Translation> {
   @override
   bool shouldReload(LocalizationsDelegate<Translation> old) => false;
 
+  ///Loads a translation JSON file of a given [Locale] form the assets
+  ///an parsed it to a [Map<dynamic, dynamic>].
   static Future<Map<dynamic, dynamic>> _loadFromAssets(Locale locale) async {
     String jsonContent = await rootBundle
         .loadString('assets/languages/${locale.languageCode}.json');
@@ -37,15 +44,41 @@ class MtsLocalizationDelegate extends LocalizationsDelegate<Translation> {
   }
 }
 
+///Holds a translation of all MyThaiStar texts for one given [Locale]
+///
+///We can access the current [Translation] like this:
+///````dart
+///Localizations.of<Translation>(context, Translation);
+/////or this
+///Translation.of(context);
+///````
+///The current [Translation] is defined by, and can be changed
+///with through the [LocalizationBloc].
+///Whenever the current [Translation] changes, all Widgets accessing it 
+///will automatically be rebuilt by the Framework.
+///
+///I did not re-implement this behavior with the bloc-pattern, because
+///handleing widget updates manually lead to a lot of boilerplate.
 @immutable
 class Translation {
-  final Locale locale;
+  ///JSON holding the translation
   final Map<dynamic, dynamic> _translationMap;
+  final Locale locale;
 
   const Translation(this.locale, this._translationMap);
 
   String get languageCode => locale.languageCode;
 
+  ///Returns translation of a given path
+  ///
+  ///paths are a shorter way of accessing the json data
+  ///stored in the [_translationMap].
+  ///````dart
+  /////instead of this:
+  ///Translation.of(context).map['menu']['header']['title'];
+  /////we can do this:
+  ///Translation.of(context).get('menu/header/title');
+  ///````
   String get(String path) {
     String result;
     try {
@@ -58,6 +91,9 @@ class Translation {
     return result;
   }
 
+  ///Gives one translation that matches the given [query]
+  ///
+  ///Recursively searches through the map
   String _findInMap(Map<dynamic, dynamic> map, List<String> query) {
     if (query.length == 1) {
       return map[query.first];
@@ -66,6 +102,7 @@ class Translation {
     }
   }
 
+  ///Enables a shorter way of accessing the current [Translation]
   static Translation of(BuildContext context) =>
       Localizations.of<Translation>(context, Translation);
 }

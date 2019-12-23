8766cfcb37c501805495744d02e08b68cb397db4
Fasust <sebastianf333@googlemail.com>
2019-12-23 14:16:43 +0100

    change dummyNumber

--- lib/data/mock/mock_order_service.dart
+++ lib/data/mock/mock_order_service.dart
@@ -2,7 +2,7 @@ import 'package:my_thai_star_flutter/models/order.dart';
 import 'package:my_thai_star_flutter/repositories/exchange_point.dart';
 
 class MockOrderService extends Service<Order, int> {
-  static const int _dummyNumber = 1;
+  static const int _dummyNumber = 404;
 
   @override
   Future<int> post(Order input) async => _dummyNumber;

9552fbc7396394da5ec2a4527326aa1866f0efe9
Sebastian Faust <sebastian.faust@smail.th-koeln.de>
2020-01-10 18:07:57 +0100

    Make service immutable

--- lib/repositories/service.dart
+++ lib/repositories/service.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
+
 ///Use-case independant interface that exchanges 
 ///[Input] for [Output] asynchronously
+@immutable
 abstract class Service<Input, Output> {
 
   ///Exchange one [Input] for a [Future<Output>]
